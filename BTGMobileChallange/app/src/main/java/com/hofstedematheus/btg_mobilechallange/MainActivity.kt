ad5978b4241d052e7f4b1382391eba67e972585b
hofstede-matheus <hofstede.matheus@gmail.com>
2021-04-16 12:46:38 -0300

    chore: init project

---
+++ BTGMobileChallange/app/src/main/java/com/hofstedematheus/btg_mobilechallange/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hofstedematheus.btg_mobilechallange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
