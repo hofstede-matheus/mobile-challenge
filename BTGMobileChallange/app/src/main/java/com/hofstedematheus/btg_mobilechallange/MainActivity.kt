5cd9ff1f9b06ba734c4bb6e6db0e6d6ef5e4068f
hofstede-matheus <hofstede.matheus@gmail.com>
2021-04-16 15:14:52 -0300

    feat(currencyScreen): create currency screen layout

--- BTGMobileChallange/app/src/main/java/com/hofstedematheus/btg_mobilechallange/MainActivity.kt
+++ BTGMobileChallange/app/src/main/java/com/hofstedematheus/btg_mobilechallange/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.hofstedematheus.btg_mobilechallange
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.appcompat.app.AppCompatActivity
+import com.hofstedematheus.btg_mobilechallange.databinding.ActivityMainBinding
+import com.hofstedematheus.btg_mobilechallange.mock.currencies
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this,
+            R.layout.support_simple_spinner_dropdown_item, currencies.map { "${it.code} - ${it.name}" }
+        )
+        binding.fromCurrencyEditText.setAdapter(adapter)
+        binding.toCurrencyEditText.setAdapter(adapter)
     }
 }
---
+++ BTGMobileChallange/app/src/main/java/com/hofstedematheus/btg_mobilechallange/mock/currencies.kt
@@ -0,0 +1,10 @@
+package com.hofstedematheus.btg_mobilechallange.mock
+
+import com.hofstedematheus.btg_mobilechallange.model.Currency
+
+val currencies = listOf(
+    Currency("AED", "United Arab Emirates Dirham"),
+    Currency("BRL", "Brazilian Real"),
+    Currency("BTC", "Bitcoin"),
+    Currency("EUR", "Euro"),
+)
